9bc310dbe5e1c8ea745ec9592f4c678a9d26586d
Ansari Suhail Mohd Saeed <ansari.m2@ugamsolutions.com>
2017-11-27 14:13:11 +0530

    emit and listen event

--- server/server.js
+++ server/server.js
@@ -1,11 +1,35 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const socketIO = require('socket.io');
+const http = require('http');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
 
-app.listen(port, (err, resp)=>{
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket)=>{
+  console.log('User connected');
+
+  socket.emit('newEmail', {
+    from: 'suhail',
+    text: 'hey! This is suhail',
+    message: 'from server'
+  });
+
+  socket.on('createEmail',(newEmail)=>{
+    console.log({newEmail});
+  });
+
+  socket.on('disconnect', ()=>{
+    console.log('User disconnected');
+  });
+});
+
+
+server.listen(port, (err, resp)=>{
   console.log(`listening on port ${port}`);
 });
