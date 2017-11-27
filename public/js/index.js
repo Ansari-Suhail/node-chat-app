7b59e4e16b59d74e98fc9c064c84f862fd144659
Ansari Suhail Mohd Saeed <ansari.m2@ugamsolutions.com>
2017-11-27 18:55:53 +0530

    commited

--- public/js/index.js
+++ public/js/index.js
@@ -8,11 +8,11 @@ socket.on('newEmail', (email)=>{
   console.log('new email', email);
 });
 
-socket.emit('createEmail', {
+/*socket.emit('createEmail', {
   from: 'ansari',
   text: 'hey! This is ansari',
   message: 'from client'
-});
+});*/
 
 socket.on('disconnect', ()=>{
   console.log('Disconnected from server');
--- server/server.js
+++ server/server.js
@@ -11,10 +11,10 @@ app.use(express.static(publicPath));
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connection', (socket)=>{
+io.on('connect', (socket)=>{
   console.log('User connected');
 
-  socket.emit('newEmail', {
+  /*socket.emit('newEmail', {
     from: 'suhail',
     text: 'hey! This is suhail',
     message: 'from server'
@@ -22,6 +22,16 @@ io.on('connection', (socket)=>{
 
   socket.on('createEmail',(newEmail)=>{
     console.log({newEmail});
+  });*/
+
+  socket.emit('newEmail',{
+    from: 'Admin',
+    text: 'Welcome to chat app'
+  });
+
+  socket.broadcast.emit('newEmail', {
+    from: 'admin',
+    text: 'new user joined'
   });
 
   socket.on('disconnect', ()=>{
