cfa51229c72b39fe5ca4af08269d5ee963646b82
Ansari Suhail Mohd Saeed <ansari.m2@ugamsolutions.com>
2017-12-06 15:57:13 +0530

    comitted till template mustache

--- public/js/index.js
+++ public/js/index.js
@@ -1,29 +1,38 @@
 var socket = io();
 
-socket.on('connect', ()=>{
+socket.on('connect', () => {
   console.log('Connected to server');
 });
 
-socket.on('disconnect', ()=>{
+socket.on('disconnect', () => {
   console.log('Disconnected from server');
 });
 
-socket.on('newMessage', function(message){
-    console.log('new message==>>',message);
-    var li = $('<li></li>');
-    li.text(`${message.from} : ${message.text}`);
-    $('#messages').append(li);
+socket.on('newMessage', function(message) {
+  console.log('new message==>>', message);
+
+  var formattedTime = moment(message.createdAt).format('h:mm a');
+  var template = $('#template').html();
+  var rendered = Mustache.render(template, {
+    from: message.from,
+    text: message.text,
+    createdAt: formattedTime
+  });
+
+  $('#messages').append(rendered);
 });
 
-$('#formID').on('submit', function(e){
+
+
+$('#formID').on('submit', function(e) {
   e.preventDefault();
 
   var messageTextBox = $('#message');
 
   socket.emit('createMessage', {
-    from : 'User',
-    text : messageTextBox.val()
-  }, function(){
+    from: 'User',
+    text: messageTextBox.val()
+  }, function() {
     messageTextBox.val('');
   });
 
--- server/server.js
+++ server/server.js
@@ -3,7 +3,9 @@ const app = express();
 const path = require('path');
 const socketIO = require('socket.io');
 const http = require('http');
-const {generateMessage} = require('./utils/message.js');
+const {
+  generateMessage
+} = require('./utils/message.js');
 
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
@@ -12,26 +14,26 @@ app.use(express.static(publicPath));
 var server = http.createServer(app);
 var io = socketIO(server);
 
-io.on('connect', (socket)=>{
+io.on('connect', (socket) => {
   console.log('User connected');
 
   socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app'));
 
   socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined'));
 
-  socket.on('createMessage', (message, callback)=>{
+  socket.on('createMessage', (message, callback) => {
     console.log('createMessage', message);
 
     io.emit('newMessage', generateMessage(message.from, message.text));
     callback();
   });
 
-  socket.on('disconnect', ()=>{
+  socket.on('disconnect', () => {
     console.log('User disconnected');
   });
 });
 
 
-server.listen(port, (err, resp)=>{
+server.listen(port, (err, resp) => {
   console.log(`listening on port ${port}`);
 });
